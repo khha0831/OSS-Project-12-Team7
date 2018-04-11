5392dc6106b964e847bb5ea4cfa1a359770f5c9c
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2018-04-11 19:27:19 +0900

    Update  calculate.c with new  result

--- calculate.c
+++ calculate.c
@@ -18,34 +18,50 @@ int main() {
   
   printf("Enter +, -, *, / : ");
   scanf("%s", &ch);
+
+  switch(ch)
+  {
+      case'+';
+      printf("%d %c %d =%d\n",num1,ch,num2,add(num1,num2));
+      result=add(num1,num2);
+      
+      case'-';
+      printf("%d %c %d =%d\n",num1,ch,num2,add(num1,num2));
+      result=mminus(num1,num2);
+      
+      case'*';
+      printf("%d %c %d =%d\n",num1,ch,num2,add(num1,num2));
+      result=mul(num1,num2);
+      
+      case'/';
+      printf("%d %c %d =%d\n",num1,ch,num2,add(num1,num2));
+      result=div(num1,num2);
+      
+      system("pause");
 }
 
-int add(int a, int b)
-{
+int add(int a, int b){
   int result1;
   
   result = a + b;
   return result1;
 }
 
-int minus(int a, int b)
-{
+int minus(int a, int b){
   int result1;
   
   result1 = a - b;
   return result1;
 }
 
-int mul(int a, int b)
-{
+int mul(int a, int b){
   int result1;
   
   result1 = a * b;
   return result1;
 }
 
-int div(int a, int b)
-{
+int div(int a, int b){
   int result1;
   
   result1 = a / b;
