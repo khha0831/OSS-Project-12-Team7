49d204dbb13219d2dee005c8a21f2bc9d06bc6af
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2018-06-01 23:48:46 +0900

    Update UserDAO.java
    DAO 파일 수정

--- UserDAO.java
+++ UserDAO.java
@@ -1,47 +1,128 @@
 package user;
 
-public class UserDTO {
-	String userID;
-	String userPW;
-	String userName;
-	String userBirthday;
-	String userGender;
-	String userEmail;
-	public String getUserID() {
-		return userID;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DatabaseUtil;
+
+public class UserDAO {
+	public int login(String userID,String userPW) {
+		String SQL="SELECT userPW FROM USER WHERE userID = ?";
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn=DatabaseUtil.getConnection();
+			pstmt =conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPW)) {
+					return 1;
+				}else {
+					return 0;
+				}
+			}
+			return -1;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		return -2;
 	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public String getUserPW() {
-		return userPW;
-	}
-	public void setUserPW(String userPW) {
-		this.userPW = userPW;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserBirthday() {
-		return userBirthday;
-	}
-	public void setUserBirthday(String userBirthday) {
-		this.userBirthday = userBirthday;
-	}
-	public String getUserGender() {
-		return userGender;
-	}
-	public void setUserGender(String userGender) {
-		this.userGender = userGender;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
+	public int join(UserDTO user) {
+		String SQL="INSERT INTO USER VALUES(?, ?, ?, ?, false)";
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn=DatabaseUtil.getConnection();
+			pstmt =conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPW());
+			pstmt.setString(3, user.getUserEmail());
+			pstmt.setString(4, user.getUserEmailHash());
+			return pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		return -1;
 	}
 	
+public String getUserEmail(String userID) {
+	String SQL="SELECT userEmail FROM USER WHERE userID=?";
+	Connection conn=null;
+	PreparedStatement pstmt = null;
+	ResultSet rs=null;
+	try {
+		conn=DatabaseUtil.getConnection();
+		pstmt =conn.prepareStatement(SQL);
+		pstmt.setString(1, userID);
+		rs=pstmt.executeQuery();
+		if(rs.next()) {
+			return rs.getString(1);
+		}	
+	}catch(Exception e) {
+		e.printStackTrace();
+	}finally {
+		try {if(conn != null)conn.close();}catch(Exception e) {e.printStackTrace();}
+		try {if(pstmt != null)pstmt.close();}catch(Exception e) {e.printStackTrace();}
+		try {if(rs != null)rs.close();}catch(Exception e) {e.printStackTrace();}
+	}
+	return null;
+	}
+	
+	
+	public boolean getUserEmailChecked(String userID) {
+		String SQL="SELECT userEmailChecked FROM USER WHERE userID=?";
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn=DatabaseUtil.getConnection();
+			pstmt =conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getBoolean(1);
+			}	
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		return false;
+	}
+	
+	public boolean setUserEmailChecked(String userID) {
+		String SQL="UPDATE USER SET userEmailChecked=true WHERE userID =?";
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn=DatabaseUtil.getConnection();
+			pstmt =conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.executeUpdate();
+			return true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close();}catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		return false;
+	}
 }
