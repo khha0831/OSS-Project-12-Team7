d1ce6124a07dac103f9588acaacc5ea92b554c75
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2018-06-02 00:01:18 +0900

    Update DatabaseUtil.java

--- DatabaseUtil.java
+++ DatabaseUtil.java
@@ -8,9 +8,9 @@ public class DatabaseUtil {
 	public static Connection getConnection() {
 		
 		try {
-			String dbURL ="jdbc:mysql://localhost:3306/MEMBER?useUnicode=true&characterEncoding=UTF-8";
-			String dbID="root";
-			String dbPW="7434";
+			String dbURL ="DB주소";
+			String dbID="아이디";
+			String dbPW="비밀번호";
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPW);
 			
